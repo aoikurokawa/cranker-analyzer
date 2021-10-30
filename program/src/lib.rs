b1d23e9fd03b39db3b25dce4f955b5865c4f9a56
Aoi <ht.so.ik0102@gmail.com>
2021-10-30 08:09:06 +0700

    first commit

---
+++ program/src/lib.rs
@@ -0,0 +1,23 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+};
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
+
+entrypoint!(process_instruction);
+
+
+
