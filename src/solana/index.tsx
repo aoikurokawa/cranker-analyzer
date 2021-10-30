c250fe58b2a1b3c9600939d7a04b59117d7ce3d9
Aoi <ht.so.ik0102@gmail.com>
2021-10-30 11:30:41 +0700

    create class

--- src/solana/index.tsx
+++ src/solana/index.tsx
@@ -14,12 +14,52 @@ const wallet = new Wallet("https://www.sollet.io", cluster);
 const programId = new PublicKey("AMThEjMzMdLBzYKnMXZVLLfTUhhCMxfrQ3L3xDsTaHdG");
 
 export async function setPayerAndBlockhashTransaction(instructions: any) {
-    const transaction = new Transaction();
-    instructions.forEach((element: any) => {
-        transaction.add(element);
+  const transaction = new Transaction();
+  instructions.forEach((element: any) => {
+    transaction.add(element);
+  });
+  transaction.feePayer = wallet.publicKey!;
+  let hash = await connection.getRecentBlockhash();
+  transaction.recentBlockhash = hash.blockhash;
+  return transaction;
+}
+
+export async function signAndSendTransaction(transaction: Transaction) {
+  try {
+    console.log("start signAndSendTransaction");
+    let signedTrans = await wallet.signTransaction(transaction);
+    let signature = await connection.sendRawTransaction(
+      signedTrans.serialize()
+    );
+    console.log("end signAndSendTransaction");
+    return signature;
+  } catch (err: any) {
+    console.log("signAndSendTransaction error", err);
+    throw err;
+  }
+}
+
+class CampaignDetails {
+  constructor(properties: any) {
+    Object.keys(properties).forEach((key, i) => {
+      // let intkey = parseInt(key);
+      this[key] = properties[key];
     });
-    transaction.feePayer = wallet.publicKey!;
-    let hash = await connection.getRecentBlockhash();
-    transaction.recentBlockhash = hash.blockhash;
-    return transaction;
+  }
+
+  static schema: any = new Map([
+    [
+      CampaignDetails,
+      {
+        kind: "struct",
+        fields: [
+          ["admin", [32]],
+          ["name", "string"],
+          ["description", "string"],
+          ["image_link", "string"],
+          ["amount_donated", "u64"],
+        ],
+      },
+    ],
+  ]);
 }
