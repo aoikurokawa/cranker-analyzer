0415ca4a50287119539cef0ce137558e90ea4790
Aoi <ht.so.ik0102@gmail.com>
2021-10-30 09:23:26 +0700

    change

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -43,11 +43,16 @@ entrypoint!(process_instruction);
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 struct CampaignDetails {
-    pub admin: Pubkey, 
-    pub name: String, 
-    pub description: String, 
-    pub image_link: String, 
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub image_link: String,
     pub amount_donated: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct WithdrawRequest {
+    pub amount: u64,
 }
 
 fn create_campaign(
@@ -55,6 +60,39 @@ fn create_campaign(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let creator_account = next_account_info(accounts_iter)?;
+
+    if !creator_account.is_signer {
+        msg!("creator_account should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    if writing_account.owner != program_id {
+        msg!("writing_account is not owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization did not worked");
+
+    if input_data.admin != *creator_account.key {
+        msg!("Invalid instruction data");
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+
+    if **writing_account.lamports.borrow() < rent_exemption {
+        msg!("The balance of writing_account should be more then rent_exemption");
+        return Err(ProgramError::InsufficientFunds);
+    }
+
+    input_data.amount_donated = 0;
+
+    input_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
+
     Ok(())
 }
 
@@ -63,6 +101,31 @@ fn withdraw(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let admin_account = next_account_info(accounts_iter)?;
+
+    if writing_account.owner != program_id {
+        msg!("writing_account is not owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    if !admin_account.is_signer {
+        msg!("admin should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+        .expect("Error deserializing data");
+
+    if campaign_data.admin != *admin_account.key {
+        msg!("Only the account admin can withdraw");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let input_data = WithdrawRequest::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization did not worked");
+
     Ok(())
 }
 
