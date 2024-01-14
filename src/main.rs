dc40e1efa94d89e8413ecf48f932aeba8936c85c
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-14 10:39:38 +0900

    feat: save keypair base58 string

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::str::FromStr;
+use std::{fs, str::FromStr};
 
 use clap::{Parser, Subcommand};
+use dotenv::dotenv;
 use solana_program::{native_token::lamports_to_sol, pubkey::Pubkey};
 use solana_rpc_client::rpc_client::RpcClient;
+use solana_sdk::{signature::Keypair, signer::Signer};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -15,9 +17,13 @@ struct Cli {
 enum Commands {
     /// print balance
     Balance { pkey: String },
+
+    /// Counter Program
+    Counter { program_id: String, pkey: String },
 }
 
 fn main() {
+    dotenv().ok();
     let cli = Cli::parse();
     let url = "https://api.devnet.solana.com";
     let rpc_client = RpcClient::new(url);
@@ -28,6 +34,31 @@ fn main() {
             let balance = rpc_client.get_balance(&pkey).expect("get balance");
 
             println!("Balance: {}SOL", lamports_to_sol(balance));
+        }
+        Commands::Counter { program_id, pkey } => {
+            let signer = initialize_keypair();
+
+            println!("Public key: {}", signer.pubkey().to_string());
+        }
+    }
+}
+
+fn initialize_keypair() -> Keypair {
+    match std::env::var("PRIVATE_KEY") {
+        Ok(private_key) => {
+            println!(
+                "found PRIVATE KEY: {}, the length is {}",
+                private_key,
+                private_key.as_bytes().len()
+            );
+            Keypair::from_base58_string(&private_key)
+        }
+        Err(_) => {
+            let signer = Keypair::new();
+            std::fs::write(".env", format!("PRIVATE_KEY={}", signer.to_base58_string()))
+                .expect("write secret key");
+
+            signer
         }
     }
 }
