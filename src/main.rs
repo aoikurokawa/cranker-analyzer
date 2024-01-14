dbb927d524c37c399abe808474545f8e3caf5176
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-14 11:25:21 +0900

    feat: ping program

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::{fs, str::FromStr};
 use clap::{Parser, Subcommand};
 use dotenv::dotenv;
 use solana_program::{
+    instruction::{AccountMeta, Instruction},
+    message::Message,
     native_token::{lamports_to_sol, LAMPORTS_PER_SOL},
     pubkey::Pubkey,
 };
 use solana_rpc_client::rpc_client::RpcClient;
-use solana_sdk::{signature::Keypair, signer::Signer};
+use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -22,7 +24,7 @@ enum Commands {
     Balance { pkey: String },
 
     /// Counter Program
-    Counter { program_id: String, pkey: Pubkey },
+    Counter { program_id: Pubkey, pkey: Pubkey },
 }
 
 fn main() {
@@ -44,6 +46,8 @@ fn main() {
             println!("Public key: {}", signer.pubkey().to_string());
 
             airdrop_sol_if_needed(&signer.pubkey(), &rpc_client);
+
+            ping_program(&rpc_client, &signer, *program_id, pkey);
             println!("Finished successfully");
         }
     }
@@ -91,4 +95,28 @@ fn airdrop_sol_if_needed(signer: &Pubkey, connection: &RpcClient) {
         let balance = connection.get_balance(&signer).expect("get balance");
         println!("New balance is {} SOL", balance / LAMPORTS_PER_SOL);
     }
+}
+
+fn ping_program(
+    connection: &RpcClient,
+    payer: &Keypair,
+    program_id: Pubkey,
+    program_data_pkey: &Pubkey,
+) {
+    let accounts = vec![AccountMeta::new(*program_data_pkey, false)];
+    let instruction = Instruction::new_with_bytes(program_id, &[], accounts);
+    let message = Message::new(&[instruction], Some(&payer.pubkey()));
+    let recent_blockhash = connection
+        .get_latest_blockhash()
+        .expect("get latest block hash");
+
+    let transaction = Transaction::new(&[payer], message, recent_blockhash);
+    let transaction_sig = connection
+        .send_and_confirm_transaction(&transaction)
+        .expect("send and confirm transaction");
+
+    println!(
+        "Transaction https://explorer.solana.com/tx/{}?cluster=devnet",
+        transaction_sig
+    );
 }
